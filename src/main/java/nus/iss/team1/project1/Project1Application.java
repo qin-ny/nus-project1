9129ab79ce97545fb5377536e7b35973107c5787
Xu Qin <1420472902@qq.com>
2022-08-27 18:33:15 +0800

    initialization

---
+++ src/main/java/nus/iss/team1/project1/Project1Application.java
@@ -0,0 +1,13 @@
+package nus.iss.team1.project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1Application.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/team1/project1/Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.team1.project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
